cd1e643467ee1a234bf838445549cf249105f420
robinzon.sokhashvili@gmail.com <robinzon.sokhashvili@gmail.com>
2024-05-13 00:52:56 +0400

    :milky_way: made version 2  with form data

--- script.js
+++ script.js
@@ -1,60 +1,67 @@
 const form = document.querySelector('form');
-const log = form.querySelector('[name="name"]');
-const pass = form.querySelector('[name="pswd"]');
 const wrapper = document.querySelector('.wrapper');
+const deleteBtn = document.querySelector('#delete_btn')
 
-let user = JSON.parse(localStorage.getItem('user')) || [];
+window.onload = () => {
+    renderList();
+};
 
-form.addEventListener('submit', account);
 
-function account(e) {
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
+form.addEventListener('submit', formEvent);
+
+function formEvent(e) {
     e.preventDefault();
-
-
-    let regex = /^[^~!#$%^&*()+\`'";:<>/\\|]+$/;
-    if (regex.test(log.value) && regex.test(pass.value) && pass.value.trim() !== '') {
-        let login = {
-            name: log.value,
-            pswd: pass.value,
-        };
+    const formData = new FormData(e.target);
+    const todoText = formData.get('todo');
     
-        user.push(login);
-        localStorage.setItem('user', JSON.stringify(user));
-        displayMessages();
     
-        log.value = '';
-        pass.value = '';
-    } else {
-        alert('Данные введены не верно. Повторите попытку ещё раз');
-    }
+    const todo = {
+        checked: false,
+        text: todoText
+    };
+
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    renderList();
+
+    e.target.reset();
 }
 
-function displayMessages() {
-    let displayMessage = '';
-    user.forEach((item, i) => {
-        displayMessage += `
-        <div id="user-${i}">
-            <label>
-                Логин: ${item.name}
-            </label>
-            <label>
-                Пароль: ${item.pswd}
-            </label>
-            <button onclick="deleteMessage(${i})">Удалить</button>
-            <input type="checkbox" id="cheked-${i}" ${item.checked ? 'checked' : ''} onchange="toggleChecked(${i})">
-        </div>
+function renderList() {
+    wrapper.innerHTML = "";
+    
+    todos.forEach((todo, i) => {
+        wrapper.innerHTML += `
+            <div id="user-${i}">
+                <label for='todo-check'>
+                    ${i + 1}. ${todo.text}
+                </label>
+                <button onclick="deleteMessage(${i})">Удалить</button>
+                <input type="checkbox" name='todo-check' id="cheked-${i}" ${todo.checked ? 'checked' : ''} onchange="toggleChecked(${i})">
+            </div>
         `;
     });
-    wrapper.innerHTML = displayMessage;
 }
 
 function deleteMessage(i) {
-    user.splice(i, 1);
-    localStorage.setItem('user', JSON.stringify(user));
-    displayMessages();
+    todos.splice(i, 1);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    renderList();
 }
 
 function toggleChecked(i) {
-    user[i].checked = !user[i].checked;
-    localStorage.setItem('user', JSON.stringify(user));
+    todos[i].checked = !todos[i].checked;
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
+
+
+deleteBtn.addEventListener('click', () => {
+    todos.forEach((todo, i) => {
+        if(todo.checked === true){
+            todos.splice(i, 1);
+        }
+    })
+    localStorage.setItem('todos', JSON.stringify(todos));
+    renderList();
+}) 
